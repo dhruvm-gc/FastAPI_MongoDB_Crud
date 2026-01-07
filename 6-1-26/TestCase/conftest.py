f3d38cbde512728497e61f7060829134e153913b
dhruvm-gc <dhruv.m@glideclouds.com>
2026-01-07 11:22:14 +0530

    updated code
    Remove comments in clean_database fixture for clarity.

--- 6-1-26/TestCase/conftest.py
+++ 6-1-26/TestCase/conftest.py
@@ -4,8 +4,8 @@ from Crud_op.database import collection
 
 @pytest.fixture(autouse=True)
 def clean_database():
-    # Runs before each test
+    
     collection.delete_many({})
     yield
-    # Runs after each test
+    
     collection.delete_many({})
